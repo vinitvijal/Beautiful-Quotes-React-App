ec2118802ff0194f8576e74459bdd22a5d0c8be7
Vinu's MacBook Pro <vinitvijal@Vinuss-MacBook-Pro.local>
2022-10-02 18:06:58 +0530

    readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Getting Started with Create React App
+# Try This Quotes App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+-> https://quotesvinu.web.app/
+
+
+
 
 ## Available Scripts
+
 
 In the project directory, you can run:
 
--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 function App() {
+   // Import the functions you need from the SDKs you need
+  // TODO: Add SDKs for Firebase products that you want to use
+   // https://firebase.google.com/docs/web/setup#available-libraries
+ 
+   // Your web app's Firebase configuration
+   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+   const firebaseConfig = {
+     apiKey: "AIzaSyDxrcMlHF9mllhOFMVLFnz-B7WOSDosj5k",
+     authDomain: "quotesvinu.firebaseapp.com",
+     projectId: "quotesvinu",
+     storageBucket: "quotesvinu.appspot.com",
+     messagingSenderId: "286825011458",
+     appId: "1:286825011458:web:e97263afcfac1697330a1b",
+     measurementId: "G-W2Q85R7SY8"
+   };
+ 
+   // Initialize Firebase
+   const app = initializeApp(firebaseConfig);
+   const analytics = getAnalytics(app);
+   console.log(analytics)
+
   const [Quote, setQuote] = useState({auth:'',message:''});
 
   useEffect(() => {
+
     quotesBag()
     console.log('Onload')
   }, []);
@@ -13,7 +36,6 @@ function App() {
     const quotesBag = async () => {
         const res = await fetch('https://api.quotable.io/random')
         const data =  await res.json()
-        console.log(data)
         setQuote({auth:data.author,message:data.content})
 
     }
